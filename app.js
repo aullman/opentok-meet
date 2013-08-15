f05d7bba01edff204f15d24497ba02e5a628afa1
Adam Ullman <adam@tokbox.com>
2013-08-15 14:42:40 +1000

    First version

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+    app = express(),
+    config;
+
+if(process.env.HEROKU) {
+    config = {
+        "port": process.env.PORT,
+        "apiKey": process.env.OTCS_API_KEY,
+        "apiSecret": process.env.OTCS_API_SECRET
+    };
+} else {
+    try {
+        config = JSON.parse(fs.readFileSync("./config.json"));
+    } catch(err) {
+        console.log("Error reading config.json - have you copied config.json.sample to config.json?");
+        process.exit();
+    }
+}
+
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(config.port, function() {
+  console.log("Listening on " + config.port);
+});
