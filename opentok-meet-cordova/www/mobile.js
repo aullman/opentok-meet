ecedd1e9dfb9eac33abaf87b5ddbeea57d6765ff
Adam Ullman <adam@tokbox.com>
2014-04-17 15:44:04 +1000

    iOS work in progress.

--- opentok-meet-cordova/www/mobile.js
+++ opentok-meet-cordova/www/mobile.js
@@ -3,6 +3,8 @@ var otmConfig = {
 },
     deferredRoom;
 
+var opentokMeet = angular.module('opentok-meet', ['opentok', 'ngRoute']);
+
 var loadRoom = function () {
     otmConfig.$http.get(otmConfig.baseURL + otmConfig.room)
       .success(function (roomData) {
@@ -25,19 +27,40 @@ var OT = {
 };
 OT.$ = window.OTHelpers;
 
+function HomeCtrl($scope, $location) {
+    $scope.joinRoom = function () {
+        $location.path('/' + $scope.room);
+    };
+}
+
+
+opentokMeet.config(['$routeProvider', function($routeProvider) {
+      $routeProvider.
+          when('/', {templateUrl: 'home.html', controller: HomeCtrl}).
+          when('/:room', {templateUrl: 'room.html',   controller: RoomCtrl}).
+          otherwise({redirectTo: '/'});
+}]);
+
 // Asynchronous fetching of the room, this is different between mobile and web because
 // We don't know the room name straight away.
-opentokMeet.factory("RoomService", ['$q', '$http', 'baseURL', function ($q, $http, baseURL) {
-    return function () {
-        deferredRoom = $q.defer();
-        otmConfig.baseURL = baseURL;
-        otmConfig.$http = $http;
-        setTimeout(function () {
-            if (!otmConfig.room) {
-                loadRoom();
-            }
-        }, 1000);
-        return deferredRoom.promise;
+opentokMeet.factory("RoomService", ['$q', '$http', 'baseURL', '$location',
+  function ($q, $http, baseURL, $location) {
+    return {
+        getRoom: function () {
+            deferredRoom = $q.defer();
+            otmConfig.baseURL = baseURL;
+            otmConfig.$http = $http;
+            setTimeout(function () {
+                if (!otmConfig.room) {
+                    otmConfig.room = $location.path().replace('/', '');
+                    loadRoom();
+                }
+            }, 1000);
+            return deferredRoom.promise;
+        },
+        changeRoom: function () {
+            $location.path('/');
+        }
     };
 }]);
 
