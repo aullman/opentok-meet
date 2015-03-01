c1a3368c54f14ccf711cc6547d9d5f1a44031f0b
Adam Ullman <adam@tokbox.com>
2015-03-01 22:02:37 +1100

    Adding some more e2e tests

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -25,12 +25,38 @@ describe('OpenTok Meet App', function() {
     });
   });
 
-  xdescribe('Room', function() {
+  describe('Room', function() {
 
     beforeEach(function() {
       browser.get('testRoom');
     });
     
+    it('should have the right title', function () {
+      expect(browser.getTitle()).toEqual('OpenTok Meet : testRoom');
+    });
+    
+    describe('publisher', function () {
+      var publisher = element(by.css('div#facePublisher'));
+      it('is displayed', function () {
+        publisher.isPresent();
+        publisher.isDisplayed();
+      });
+      
+      it('contains a video element', function () {
+        expect(publisher.element(by.css('video'))).toBeDefined();
+      });
+      
+      it('moves when it is dragged', function (done) {
+        publisher.getLocation().then(function (oldLocation) {
+          browser.actions().dragAndDrop(publisher, element(by.css('body'))).perform();
+          publisher.getLocation().then(function (newLocation) {
+            expect(newLocation.x).not.toBe(oldLocation.x);
+            expect(newLocation.y).not.toBe(oldLocation.y);
+            done();
+          });
+        });
+      });
+    });
     
   });
 });
