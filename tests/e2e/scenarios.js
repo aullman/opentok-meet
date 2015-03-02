1d9a7b294c6a3aec0de478eb648da1ded4997d99
Adam Ullman <adam@tokbox.com>
2015-03-02 23:12:51 +1100

    #27 added a bunch of e2e tests
    Now we have 20 tests with 53 assertions.

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -34,29 +34,219 @@ describe('OpenTok Meet App', function() {
     it('should have the right title', function () {
       expect(browser.getTitle()).toEqual('OpenTok Meet : testRoom');
     });
-    
+
+    it('should have a loader being displayed', function () {
+      expect(element(by.css('#loader')).isDisplayed()).toBe(true);
+    });
+
+    it('should show a shareInfo message when you connect', function () {
+      browser.wait(function () {
+        return element(by.css('#shareInfo')).isPresent();
+      }, 10000);
+    });
+
     describe('publisher', function () {
       var publisher = element(by.css('div#facePublisher'));
       it('is displayed', function () {
-        publisher.isPresent();
-        publisher.isDisplayed();
+        expect(publisher.isPresent()).toBe(true);
+        expect(publisher.isDisplayed()).toBe(true);
       });
       
-      it('contains a video element', function () {
-        expect(publisher.element(by.css('video'))).toBeDefined();
+      it('contains a video element and is HD', function () {
+        browser.wait(function () {
+          return element(by.css('.OT_publisher:not(.OT_loading)')).isPresent();
+        }, 10000);
+        var publisherVideo = publisher.element(by.css('video'));
+        expect(publisherVideo).toBeDefined();
+        expect(publisherVideo.getAttribute('videoWidth')).toBe('1280');
+        expect(publisherVideo.getAttribute('videoHeight')).toBe('720');
       });
       
-      it('moves when it is dragged', function (done) {
-        publisher.getLocation().then(function (oldLocation) {
-          browser.actions().dragAndDrop(publisher, element(by.css('body'))).perform();
-          publisher.getLocation().then(function (newLocation) {
-            expect(newLocation.x).not.toBe(oldLocation.x);
-            expect(newLocation.y).not.toBe(oldLocation.y);
-            done();
+      it('moves when it is dragged', function () {
+        var oldLocation = publisher.getLocation();
+        browser.actions().dragAndDrop(publisher, element(by.css('body'))).perform();
+        var newLocation = publisher.getLocation();
+        expect(newLocation).not.toEqual(oldLocation);
+      });
+    });
+
+    describe('bottomBar', function () {
+      describe('publish buttons', function () {
+        var publishBtn = element(by.css('#publishBtn')),
+          publishSDBtn = element(by.css('#publishSDBtn'));
+
+        it('publishBtn is red and being displayed publishSDBtn is not present', function () {
+          expect(publishBtn.isPresent()).toBe(true);
+          expect(publishBtn.getAttribute('class')).toContain('red');
+          expect(publishSDBtn.isPresent()).toBe(false);
+        });
+
+        it('publishBtn toggles the publisher when clicked and goes between green and red and publishes in HD', function () {
+          var publisher = element(by.css('div#facePublisher'));
+          expect(publisher.isPresent()).toBe(true);
+          publishBtn.click();
+          expect(publisher.isPresent()).toBe(false);
+          expect(publishBtn.getAttribute('class')).toContain('green');
+          publishBtn.click();
+          expect(publisher.isPresent()).toBe(true);
+          expect(publishBtn.getAttribute('class')).toContain('red');
+          browser.wait(function () {
+            return element(by.css('.OT_publisher:not(.OT_loading)')).isPresent();
+          }, 10000);
+          var publisherVideo = publisher.element(by.css('video'));
+          expect(publisherVideo.getAttribute('videoWidth')).toBe('1280');
+          expect(publisherVideo.getAttribute('videoHeight')).toBe('720');
+        });
+        
+        it('publishSDBtn shows up when you unpublish and can publish', function () {
+          var publisher = element(by.css('div#facePublisher'));
+          publishBtn.click();
+          expect(publishSDBtn.isPresent()).toBe(true);
+          publishSDBtn.click();
+          expect(publisher.isPresent()).toBe(true);
+          browser.wait(function () {
+            return element(by.css('.OT_publisher:not(.OT_loading)')).isPresent();
+          }, 10000);
+          var publisherVideo = publisher.element(by.css('video'));
+          expect(publisherVideo.getAttribute('videoWidth')).toBe('640');
+          expect(publisherVideo.getAttribute('videoHeight')).toBe('480');
+        });
+      });
+
+      describe('showWhiteboardBtn', function () {
+        var showWhiteboardBtn = element(by.css('#showWhiteboardBtn'));
+
+        it('is green and being displayed', function () {
+          expect(showWhiteboardBtn.isPresent()).toBe(true);
+          expect(showWhiteboardBtn.getAttribute('class')).toContain('green');
+        });
+
+        it('toggles the whiteboard when you click it and the button goes between green and red', function () {
+          browser.wait(function () {
+            return element(by.css('ot-whiteboard')).isPresent();
+          }, 10000);
+          var whiteboard = element(by.css('ot-whiteboard'));
+          expect(whiteboard.isPresent()).toBe(true);
+          expect(whiteboard.isDisplayed()).toBe(false);
+          showWhiteboardBtn.click();
+          expect(showWhiteboardBtn.getAttribute('class')).toContain('red');
+          expect(whiteboard.isDisplayed()).toBe(true);
+          showWhiteboardBtn.click();
+          expect(showWhiteboardBtn.getAttribute('class')).toContain('green');
+          expect(whiteboard.isDisplayed()).toBe(false);
+        });
+      });
+
+      describe('showEditorBtn', function () {
+        var showEditorBtn = element(by.css('#showEditorBtn'));
+
+        it('is green and being displayed', function () {
+          expect(showEditorBtn.isPresent()).toBe(true);
+          expect(showEditorBtn.getAttribute('class')).toContain('green');
+        });
+
+        it('toggles the editor when you click it and the button goes between green and red', function () {
+          browser.wait(function () {
+            return element(by.css('ot-editor')).isPresent();
+          }, 10000);
+          var editor = element(by.css('ot-editor'));
+          expect(editor.isPresent()).toBe(true);
+          expect(editor.isDisplayed()).toBe(false);
+          showEditorBtn.click();
+          expect(editor.isDisplayed()).toBe(true);
+          expect(showEditorBtn.getAttribute('class')).toContain('red');
+          showEditorBtn.click();
+          expect(editor.isDisplayed()).toBe(false);
+          expect(showEditorBtn.getAttribute('class')).toContain('green');
+        });
+      });
+
+      describe('startArchiveBtn', function () {
+        var startArchiveBtn = element(by.css('#startArchiveBtn'));
+        beforeEach(function () {
+          browser.wait(function () {
+            return startArchiveBtn.isPresent();
+          }, 10000);
+        });
+
+        it('is present and green', function () {
+          expect(startArchiveBtn.isPresent()).toBe(true);
+          expect(startArchiveBtn.getAttribute('class')).toContain('green');
+        });
+
+        it('toggles archiving when you click it and adds an archiving status message', function () {
+          startArchiveBtn.click();
+          expect(startArchiveBtn.getAttribute('class')).toContain('red');
+          // Wait for the archiving light to show up in the Publisher
+          browser.wait(function () {
+            return element(by.css('.OT_publisher .OT_archiving-on')).isPresent();
+          }, 10000);
+          expect(element(by.css('#archiveStatus')).isPresent()).toBe(true);
+          startArchiveBtn.click();
+          expect(startArchiveBtn.getAttribute('class')).toContain('green');
+          // Wait for the archiving light to turn off
+          browser.wait(function () {
+            return element(by.css('.OT_publisher .OT_archiving-off')).isPresent();
+          }, 10000);
+        });
+      });
+
+      describe('screenshare button', function () {
+        var screenShareBtn = element(by.css('#showscreen'));
+        // Can't test screen sharing properly right now because the extension isn't registered for localhost
+        it('shows an install prompt when you click it and it changes colour', function () {
+          expect(element(by.css('#installScreenshareExtension')).isPresent()).toBe(false);
+          expect(screenShareBtn.getAttribute('class')).toContain('green');
+          screenShareBtn.click();
+          expect(screenShareBtn.getAttribute('disabled')).toBe('true');
+          browser.wait(function () {
+            return element(by.css('#installScreenshareExtension')).isPresent();
+          }, 10000);
+        });
+      });
+
+      describe('changeRoom button', function () {
+        var changeRoomBtn = element(by.css('#changeRoom'));
+
+        beforeEach(function () {
+          // Wait until we're connected
+          browser.wait(function () {
+            return element(by.css('div.session-connected')).isPresent();
           });
+        });
+
+        it('takes you back to the login screen if you click it', function (done) {
+          changeRoomBtn.click();
+          setTimeout(function () {
+            expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl);
+            done();
+          }, 2000);
         });
       });
     });
-    
+  });
+
+  describe('2 browsers in the same room', function () {
+    var secondBrowser;
+    beforeEach(function () {
+      browser.get('testRoom');
+      secondBrowser = browser.forkNewDriverInstance();
+      secondBrowser.get('testRoom');
+    });
+
+    it('should subscribe to one another and display a video element with the right videoWidth and videoHeight', function () {
+      var firstSubscriberVideo = element(by.css('.OT_subscriber:not(.OT_loading) video'));
+      var secondSubscriberVideo = secondBrowser.element(by.css('.OT_subscriber:not(.OT_loading) video'));
+      browser.wait(function () {
+        return firstSubscriberVideo.isPresent();
+      });
+      secondBrowser.wait(function () {
+        return secondSubscriberVideo.isPresent();
+      });
+      expect(firstSubscriberVideo.getAttribute('videoWidth')).toBe('1280');
+      expect(firstSubscriberVideo.getAttribute('videoHeight')).toBe('720');
+      expect(secondSubscriberVideo.getAttribute('videoWidth')).toBe('1280');
+      expect(secondSubscriberVideo.getAttribute('videoHeight')).toBe('720');
+    });
   });
 });
