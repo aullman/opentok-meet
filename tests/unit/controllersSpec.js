1a3403e602a5609e1c99bfca2c03a2b01b6bf007
Adam Ullman <adam@tokbox.com>
2015-03-10 14:19:50 +1100

    #27 Added archiving unit tests

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -6,11 +6,13 @@ describe('OpenTok Meet controllers', function() {
     var ctrl,
       scope,
       RoomServiceMock,
-      windowMock;
+      windowMock,
+      $httpBackend,
+      roomDefer;
 
     beforeEach(module('opentok-meet'));
     
-    beforeEach(inject(function($controller, $rootScope, $q) {
+    beforeEach(inject(function($controller, $rootScope, $q, $injector) {
       scope = $rootScope.$new();
       scope.session = {
         connection: {
@@ -21,13 +23,14 @@ describe('OpenTok Meet controllers', function() {
         changeRoom: function () {
         },
         getRoom: function () {
-          var deferred = $q.defer();
-          return deferred.promise;
+          roomDefer = $q.defer();
+          return roomDefer.promise;
         }
       };
       windowMock = jasmine.createSpyObj('$window', ['addEventListener']);
       documentMock = {context: { body: jasmine.createSpyObj('body', ['addEventListener'])}};
-      ctrl = $controller('RoomCtrl', {$scope:scope, $http: {}, $window: windowMock, 
+      $httpBackend = $injector.get('$httpBackend');
+      ctrl = $controller('RoomCtrl', {$scope:scope, $window: windowMock, 
         $document: documentMock, $timeout: {}, OTSession: {}, RoomService: RoomServiceMock, baseURL: ''});
     }));
 
@@ -43,6 +46,63 @@ describe('OpenTok Meet controllers', function() {
       expect(scope.notMine).toBeDefined();
       expect(scope.notMine({connection: {connectionId: 'someOtherConnectionId'}})).toEqual(true);
       expect(scope.notMine({connection: {connectionId: 'mockConnectionId'}})).toEqual(false);
+    });
+
+    describe('togglePublish', function () {
+      it('publishes with HD properties', function () {
+        scope.togglePublish(true);
+        expect(scope.publishing).toBe(true);
+        expect(scope.facePublisherProps.resolution).toBe('1280x720');
+        scope.togglePublish(true);
+        expect(scope.publishing).toBe(false);
+      });
+      it('publishes with SD properties', function () {
+        scope.togglePublish(false);
+        expect(scope.publishing).toBe(true);
+        expect(scope.facePublisherProps.resolution).not.toBeDefined();
+      });
+    });
+
+    describe('toggleArchiving', function () {
+      it('toggles the archiving property', function () {
+        expect(scope.archiving).toBe(false);
+        scope.toggleArchiving();
+        expect(scope.archiving).toBe(true);
+        scope.toggleArchiving();
+        expect(scope.archiving).toBe(false);
+      });
+      it('posts to /startArchive and /stopArchive', function () {
+        $httpBackend.expectPOST('undefined/startArchive')
+          .respond(200, '{"archiveId": "mockArchiveId"}');
+        scope.toggleArchiving();
+        $httpBackend.flush();
+        expect(scope.archiveId).toBe('mockArchiveId');
+        expect(scope.archiving).toBe(true);
+
+        $httpBackend.expectPOST('undefined/stopArchive', {archiveId: 'mockArchiveId'})
+          .respond(200, '{"archiveId": "mockArchiveId"}');
+        scope.toggleArchiving();
+        $httpBackend.flush();
+        expect(scope.archiving).toBe(false);
+      });
+      it('handles errors from /startArchive', function () {
+        $httpBackend.expectPOST('undefined/startArchive')
+          .respond(200, '{"error": "mock error"}');
+        scope.toggleArchiving();
+        $httpBackend.flush();
+        expect(scope.archiving).toBe(false);
+      });
+      it('handles errors from /stopArchive', function () {
+        $httpBackend.expectPOST('undefined/startArchive')
+          .respond(200, '{"archiveId": "mockArchiveId"}');
+        scope.toggleArchiving();
+        $httpBackend.flush();
+        $httpBackend.expectPOST('undefined/stopArchive', {archiveId: 'mockArchiveId'})
+          .respond(200, '{"error": "mock error message"}');
+        scope.toggleArchiving();
+        $httpBackend.flush();
+        expect(scope.archiving).toBe(true);
+      });
     });
   });
 });
