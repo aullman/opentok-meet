58e19c6efe719d361da7a411bb7dbdc37b05252e
Adam Ullman <adam@tokbox.com>
2015-03-16 10:24:45 +1100

    #27 - Run single run test for Travis by default

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Chrome', 'Firefox'],
 
     plugins : [
             'karma-chrome-launcher',
