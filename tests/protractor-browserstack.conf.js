6edd68a1b4b1ec24617c55332464671c3d00c735
Adam Ullman <adam@tokbox.com>
2015-08-03 10:39:56 +1000

    Run protractor tests in browserstack from Travis
    First try.

---
+++ tests/protractor-browserstack.conf.js
@@ -0,0 +1,27 @@
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'e2e/*.js'
+  ],
+
+  capabilities: {
+    'browserstack.user' : process.env.BROWSERSTACK_USER,
+    'browserstack.key' : process.env.BROWSERSTACK_KEY,
+    'browserstack.local' : 'true',
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['use-fake-device-for-media-stream', 'use-fake-ui-for-media-stream']
+    }
+  },
+  
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+
+  baseUrl: 'http://localhost:5000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 60000
+  }
+};
