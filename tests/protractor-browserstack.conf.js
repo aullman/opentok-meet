f36bced8e755ed922e77f041b25d4aebb7220271
Adam Ullman <adam@tokbox.com>
2015-08-03 11:07:18 +1000

    Travis is running localserver on 3000

--- tests/protractor-browserstack.conf.js
+++ tests/protractor-browserstack.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   
   seleniumAddress: 'http://hub.browserstack.com/wd/hub',
 
-  baseUrl: 'http://localhost:5000/',
+  baseUrl: 'http://localhost:3000/',
 
   framework: 'jasmine',
 
