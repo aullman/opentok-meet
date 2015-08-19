43166639946d687d7ada35a666bdeb51019e58be
Adam Ullman <adam@tokbox.com>
2015-08-19 18:18:43 +1000

    Use the right port for browserstack Firefox

--- tests/protractor-browserstack-firefox.conf.js
+++ tests/protractor-browserstack-firefox.conf.js
@@ -11,7 +11,7 @@ exports.config = {
 
   seleniumAddress: 'http://hub.browserstack.com/wd/hub',
 
-  baseUrl: 'https://localhost:5000/',
+  baseUrl: 'http://localhost:3000/',
 
   framework: 'jasmine',
 
