ec4fc8b81fec38a4500acab62751e906b39a990c
Adam Ullman <adam@tokbox.com>
2015-09-04 14:11:14 +1000

    Just run chrome on Browserstack for now Splitting out into multiple scripts

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -77,29 +77,27 @@ switch(process.env.BROWSER) {
       ],
 
       capabilities: {
+        'browserstack.user' : process.env.BROWSERSTACK_USERNAME,
+        'browserstack.key' : process.env.BROWSERSTACK_KEY,
+        'browserstack.local' : 'true',
         'browserName': 'chrome',
+        'os' : 'OS X',
+        'os_version' : 'Yosemite',
         'chromeOptions': {
-          'args': ['auto-select-desktop-capture-source="Entire screen"',
-            'use-fake-device-for-media-stream',
-            'use-fake-ui-for-media-stream'],
-          'binary': process.env.CHROME_BIN
+          'args': ['use-fake-device-for-media-stream', 'use-fake-ui-for-media-stream']
         }
       },
 
-      directConnect: true,
+      seleniumAddress: 'http://hub.browserstack.com/wd/hub',
 
       baseUrl: 'http://localhost:5000/',
-
-      params: {
-        testScreenSharing: false
-      },
 
       framework: 'jasmine',
 
       jasmineNodeOpts: {
-        defaultTimeoutInterval: 30000
+        defaultTimeoutInterval: 60000
       }
-    };
+  };
   break;
 }
 exports.config = config;
