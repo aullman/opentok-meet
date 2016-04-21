bff559abb6a72b820124930ccf63e20df1858212
Adam Ullman <adam@tokbox.com>
2016-04-21 16:36:25 +1000

    No need for the devices check anymore

--- tests/e2e/iesmoketest.js
+++ tests/e2e/iesmoketest.js
@@ -5,34 +5,6 @@
 // the other tests don't pass consistently there
 var uuid = require('uuid');
 describe('IE Smoke Test', function() {
-  it('has the plugin installed and audio, video input devices', function (done) {
-    // We need to wait a little bit for the tests before starting. This is to allow
-    // the Plugin installer to finish in IE.
-    var videoInput = element(by.css('.videoInput'));
-    var audioInput = element(by.css('.audioInput'));
-    var waitToStart = function () {
-      browser.get('/test/devices.html');
-      browser.wait(function () {
-        return element(by.css('body.loaded')).isPresent();
-      }, 4000).then(function () {
-        videoInput.isPresent().then(function (hasVideoInput) {
-          if (hasVideoInput) {
-            audioInput.isPresent().then(function (hasAudioInput) {
-              if (hasAudioInput) {
-                done();
-              } else {
-                setTimeout(waitToStart, 2000);
-              }
-            });
-          } else {
-            setTimeout(waitToStart, 2000);
-          }
-        });
-      });
-    };
-    waitToStart();
-  });
-
   it('can connect, publish and subscribe', function () {
     var roomName = uuid.v1() + 'p2p';
     browser.get(roomName);
