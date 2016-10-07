f1730f20c056d5a53cf7776ba85bc781b3998cd4
Adam Ullman <adam@tokbox.com>
2016-10-07 15:36:50 +1100

    Put roomName back in

--- tests/e2e/login.js
+++ tests/e2e/login.js
@@ -2,8 +2,14 @@
 /* global browser: false */
 /* global element: false */
 /* global by: false */
-describe('Login', function() {
+var uuid = require('uuid');
+fdescribe('Login', function() {
+  var roomName;
   beforeEach(function () {
+    while(!roomName || roomName.indexOf('p2p') > -1) {
+      // Don't want the roomname to have p2p in it or it will be a p2p room
+      roomName = uuid.v1();
+    }
     browser.getCapabilities().then(function (cap) {
       browser.browserName = cap.get('browserName');
     });
