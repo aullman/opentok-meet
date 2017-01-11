707229abd577c240d3f39f898518d574319d1b78
Aiham Hammami <aiham@aiham.net>
2017-01-11 17:19:11 +1100

    OPENTOK-31614 - Add commit hash to sentry tags

--- src/js/lib/error-reporting.js
+++ src/js/lib/error-reporting.js
@@ -6,6 +6,9 @@ module.exports = function installErrorReporting() {
       'https://32febef98a3140ceb21ad35138178163@sentry.io/118721',
       {
         release: VERSION,
+        tags: {
+          commitHash: COMMITHASH
+        },
         debug: true
       }
     )
