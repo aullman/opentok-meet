a0a0092ba836cb65f27c29a402e049df9222a383
Adam Ullman <adam@tokbox.com>
2017-03-07 23:03:38 +1100

    Got the text chat working. Just need to add tests.

--- src/js/app.js
+++ src/js/app.js
@@ -8,9 +8,11 @@ require('opentok-whiteboard');
 require('opentok-whiteboard/opentok-whiteboard.css');
 require('opentok-editor');
 require('opentok-editor/opentok-editor.css');
+require('opentok-textchat');
+require('opentok-textchat/opentok-textchat.css');
 
 angular.module('opentok-meet', ['opentok', 'opentok-whiteboard',
-  'opentok-editor']);
+  'opentok-editor', 'opentok-textchat']);
 
 require('./directives.js');
 require('./sync-click.js');
