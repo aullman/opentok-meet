c9fe9756230c761f1253d0725c49290fc329f1d1
Adam Ullman <adam@tokbox.com>
2017-11-08 09:23:12 +1100

    fixes #190 - broadcast otLayout on rootScope

--- src/js/directives.js
+++ src/js/directives.js
@@ -196,7 +196,7 @@ function($document, $window) {
       }
     };
   }])
-  .directive('expandButton', function () {
+  .directive('expandButton', ['$rootScope', function ($rootScope) {
     return {
       restrict: 'E',
       template: '<button class="resize-btn ion-arrow-expand" ng-click="$emit(\'changeSize\');"' +
@@ -210,10 +210,10 @@ function($document, $window) {
             scope.expanded = !scope.expanded;
           }
           scope.$apply();
-          scope.$broadcast('otLayout');
+          $rootScope.$broadcast('otLayout');
         };
         angular.element(element).on('click', toggleExpand);
         angular.element(element).parent().on('dblclick', toggleExpand);
       }
     };
-  });
+  }]);
