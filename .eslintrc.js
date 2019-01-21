ffeff45ece7bc645d1effb754fbee614c3dcdee2
Adam Ullman <adam@tokbox.com>
2019-01-21 16:35:53 +1100

    Fix eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
       "no-console": 0,
       "no-param-reassign": 0, // In angular you do a lot of scope.something = something
       "comma-dangle": ["error", {
-        "functions": "ignore",
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        // this is disabled as we run our codebase through node
+        "functions": "never"
       }],
     },
     "env": {
