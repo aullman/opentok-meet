d8fad04ea9bee813afd7f54a3730a23d2ebcc34d
Adam Ullman <adam@tokbox.com>
2019-01-22 11:59:00 +1100

    Use filterous instaFilters and show a preview of what they look like

--- src/js/filter-picker.js
+++ src/js/filter-picker.js
@@ -1,13 +1,53 @@
 const filterPickerHTML = require('../templates/filter-picker.html');
 require('../css/filter-picker.css');
+const filterous = require('filterous/lib/instaFilters');
 
 function FilterPickerDirective() {
   function link(scope, element) {
+    let currentPublisher;
     scope.showFilterList = false;
-    scope.filters = ['none', 'sepia', 'invert', 'sketch', 'grayscale', 'blur'];
+    scope.filters = ['none', 'brannan', 'xpro2', 'moon', 'nashville', 'lofi', 'helena'];
+    scope.filterImages = null;
 
     scope.toggleFilterList = () => {
       scope.showFilterList = !scope.showFilterList;
+
+      setTimeout(() => {
+        // Do it asynchronously so we don't block
+        if (!scope.filterImages) {
+          const videoElement = currentPublisher.element.querySelector('video');
+          if (videoElement) {
+            scope.filterImages = {};
+            const canvas = document.createElement('canvas');
+            canvas.width = videoElement.videoWidth;
+            canvas.height = videoElement.videoHeight;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+            const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            scope.filters.forEach((filter) => {
+              setTimeout(() => {
+                if (filterous[filter]) {
+                  // Make a copy of the ImageData so we don't modify it
+                  const imgDataCopy = new ImageData(
+                    new Uint8ClampedArray(imgData.data),
+                    imgData.width, imgData.height
+                  );
+                  const filteredImgData = filterous[filter](imgDataCopy);
+                  const tmpCanvas = document.createElement('canvas');
+                  tmpCanvas.width = videoElement.videoWidth;
+                  tmpCanvas.height = videoElement.videoHeight;
+                  const tmpCtx = tmpCanvas.getContext('2d');
+                  tmpCtx.putImageData(filteredImgData, 0, 0);
+                  scope.filterImages[filter] = `${tmpCanvas.toDataURL('image/png')}`;
+                } else {
+                  scope.filterImages[filter] = `${canvas.toDataURL('image/png')}`;
+                }
+                scope.$apply();
+              });
+            });
+          }
+        }
+      });
     };
 
     scope.selectFilter = (f) => {
@@ -23,12 +63,21 @@ function FilterPickerDirective() {
         scope.$apply();
       }
     });
+
+    scope.$watch('publisher', (newValue) => {
+      if (newValue === undefined) {
+        return;
+      }
+
+      currentPublisher = newValue;
+    });
   }
 
   return {
     restrict: 'E',
     scope: {
       filter: '=',
+      publisher: '=',
     },
     template: filterPickerHTML,
     link,
